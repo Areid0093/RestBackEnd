524b745be7d662b7bc7bd231ffa415ae2f7d0659
Joellehelm <joellenhelm@gmail.com>
2019-12-02 11:18:45 -0600

    back end user authentication added

--- RestaurantBackEnd/app/models/user.rb
+++ RestaurantBackEnd/app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :reviews
     has_many :restaurants, through: :reviews
+
+    validates: :username, presence: true
+    validates: :username, uniqueness: true
+    validates: :email, presence: true
+    validates: :email, uniqueness: true
+    
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+end
+
 end
