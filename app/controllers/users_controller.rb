521324a8fa4adae6e1b06973d41234b1d264c5d1
Alec Reid <alecreid0093@gmail.com>
2019-12-02 14:33:18 -0600

    authentication fix

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
+    # def index
+    #     @users = User.all
+    #     if @users
+    #       render json: {
+    #         users: @users
+    #       }
+    #     else
+    #       render json: {
+    #         status: 500,
+    #         errors: ['no users found']
+    #       }
+    #     end
+    # end
+
+
+    def show
+        @user = User.find(params[:id])
+       if @user
+          render json: {
+            user: @user
+          }
+        else
+          render json: {
+            status: 500,
+            errors: ['user not found']
+          }
+        end
+      end
+      
+    def new
+      @user = User.new(user_params)
+    end
+
+
+      def create
+        @user = User.create(user_params)
+        if @user.valid?
+          @token = encode_token({ user_id: @user.id })
+          render json: { user: @user, jwt: @token }, status: :created
+        else
+          render json: { error: 'failed to create user' }, status: :not_acceptable
+        end
+      end
+      
+    private
+      
+      def user_params
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+      end
+    end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :favorites
+  resources :reviews
+  resources :restaurants
+  
+  
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'application#logged_in?'
+  
+  resources :users, only: [:create]
+end
